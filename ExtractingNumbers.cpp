e69e6cb1d5a39d689b803c06be961fd1fecef430
Anshika Singla <71587985+Anshi-1234@users.noreply.github.com>
2022-07-07 11:19:19 +0530

    Update ExtractingNumbers.cpp

--- ExtractingNumbers.cpp
+++ ExtractingNumbers.cpp
@@ -2,10 +2,12 @@ long Solution::solve(string A) {
     string temp="";
     long sum=0;
     for(char ch:A){
+        //checking if it is a digit concatenating strings until you find any character
        if(isdigit(ch))
        temp+=ch;
        else
        {
+           //Atoi() -> Convert string to integer
            sum+=atoi(temp.c_str());
            temp="";
        }
